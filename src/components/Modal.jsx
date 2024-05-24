22d49fc90f62bf558ee391ae75b3f9e24459d58b
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-05-24 16:15:37 -0300

    Nuevo componente modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,86 +1,162 @@
 import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-const Modal = ({ title, subtitle, children, handelModal, show, classContainer = 'w-full max-w-3xl min-h-96', onClose }) => {
+const Modal = ({
+  children,
+  size = 'md',
+  radius = 'lg',
+  shadow = 'lg',
+  backdrop = 'opaque',
+  scrollBehavior = 'normal',
+  placement = 'auto',
+  isOpen,
+  defaultOpen,
+  isDismissable = true,
+  isKeyboardDismissDisabled = false,
+  shouldBlockScroll = true,
+  hideCloseButton = false,
+  closeButton,
+  portalContainer = document.body,
+  classNames: customClassNames = {},
+  onOpenChange,
+  onClose,
+  title,
+  subtitle,
+}) => {
+  const [isModalOpen, setIsModalOpen] = React.useState(defaultOpen || false);
 
-  const closeModal = () => {
-    handelModal();
-    if(onClose){
+  useEffect(() => {
+    if (isOpen !== undefined) {
+      setIsModalOpen(isOpen);
+      if (onOpenChange) {
+        onOpenChange(isOpen);
+      }
+    }
+  }, [isOpen, onOpenChange]);
+
+  useEffect(() => {
+    if (isModalOpen && shouldBlockScroll) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isModalOpen, shouldBlockScroll]);
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+    if (onOpenChange) {
+      onOpenChange(false);
+    }
+    if (onClose) {
       onClose();
+    }
+  }
+
+  const handleBackdropClose = () => {
+    if (isDismissable) {
+      setIsModalOpen(false)
     }
   };
 
-  const stopPropagation = (e) => {
-    e.stopPropagation(); // Evita que el evento se propague al contenedor interno
+  const handleKeyDown = (e) => {
+    if (e.key === 'Escape' && !isKeyboardDismissDisabled) {
+      handleClose();
+    }
   };
 
   useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        closeModal();
-      }
-    };
-
-    if (show) {
-      document.addEventListener('keydown', handleKeyDown);
+    if (isModalOpen) {
+      window.addEventListener('keydown', handleKeyDown);
     } else {
-      document.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     }
 
-    // Cleanup event listener on component unmount or when show changes
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [show]);
+  }, [isModalOpen, isKeyboardDismissDisabled]);
 
-  return (
-    <>
-      {show && (
-        <div
-          tabIndex="-1"
-          aria-hidden="true"
-          onClick={closeModal}
-          className={`fixed top-0 flex left-0 right-0 z-50 h-full min-h-screen w-full bg-neutral-900/50 dark:bg-neutral-900/70  overflow-x-hidden overflow-y-auto md:inset-0 justify-center items-center bg-opacity-75 backdrop-filter backdrop-blur-sm`}
-        >
-          <div
-            name="modalContainer"
-            onClick={stopPropagation}
-            className={`relative bg-neutral-100 dark:bg-neutral-800 p-6 rounded-lg max-h-screen overflow-y-auto ${classContainer} tmn-fadeIn transition-all duration-700 ease-in-out`}
-          >
-            <div name="divPadre" className="relative mx-auto text-center">
-              <div name="encabezado" className="flex flex-col border-b rounded-t border-neutral-200 dark:border-neutral-700">
-                <div name="header" className="flex flex-col items-start sm:flex-row sm:justify-between sm:items-center px-2 pb-3 rounded-t ">
-                  <div name="titleContainer" className="flex flex-col w-full">
-                    {title && (
-                      <h3 id="form_title" name="title" className="text-lg font-medium text-left text-neutral-600 dark:text-white leading-none">
-                        {title}
-                      </h3>
-                    )}
-                    {subtitle && (
-                      <p id="form_subtitle" name="subtitle" className="mt-1 text-sm text-left font-normal text-neutral-500 dark:text-neutral-400 leading-tight">
-                        {subtitle}
-                      </p>
-                    )}
-                  </div>
-                  <div>
-                    <button
-                      type="button"
-                      className="text-neutral-400 bg-transparent hover:bg-red-200 hover:text-red-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-red-600 dark:hover:text-white"
-                      onClick={closeModal}
-                    >
-                      <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
-                      </svg>
-                      <span className="sr-only">Cerrar modal</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className='p-2 text-neutral-500 dark:text-neutral-400 text-left'>{children}</div>
-            </div>
+  const modalSizes = {
+    xs: 'max-w-xs',
+    sm: 'max-w-sm',
+    md: 'max-w-md',
+    lg: 'max-w-lg',
+    xl: 'max-w-xl',
+    '2xl': 'max-w-2xl',
+    '3xl': 'max-w-3xl',
+    '4xl': 'max-w-4xl',
+    '5xl': 'max-w-5xl',
+    full: 'w-full h-full',
+  };
+
+  const modalRadius = {
+    none: 'rounded-none',
+    sm: 'rounded-sm',
+    md: 'rounded-md',
+    lg: 'rounded-lg',
+  };
+
+  const modalShadows = {
+    none: 'shadow-none',
+    sm: 'shadow-sm',
+    md: 'shadow-md',
+    lg: 'shadow-lg',
+  };
+
+  const modalBackdrops = {
+    transparent: 'bg-transparent',
+    opaque: 'bg-black bg-opacity-50',
+    blur: 'backdrop-filter backdrop-blur-sm bg-black bg-opacity-50',
+  };
+
+  const modalPlacements = {
+    auto: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
+    top: 'top-0 left-1/2 transform -translate-x-1/2',
+    center: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
+    bottom: 'bottom-0 left-1/2 transform -translate-x-1/2',
+  };
+
+  const wrapperClasses = `${modalBackdrops[backdrop]} fixed inset-0 z-50 ${customClassNames.wrapper || ''}`;
+
+  const baseClasses = `${modalSizes[size]} ${modalRadius[radius]} ${modalShadows[shadow]} ${modalPlacements[placement]} dark:text-white bg-white dark:bg-zinc-900 relative ${customClassNames.base || ''}`;
+
+  if (!isModalOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={wrapperClasses} onClick={handleBackdropClose}>
+      <div
+        className={baseClasses}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {!hideCloseButton && (
+          <div className={`absolute top-1 right-1 ${customClassNames.closeButton || ''}`}>
+            {closeButton || (
+              <button
+                onClick={handleClose}
+                role="button"
+                aria-label="Close"
+                className="appearance-none select-none p-2 text-zinc-500 rounded-full hover:bg-zinc-100 dark:hover:bg-zinc-800 outline-none"
+                type="button">
+                <svg aria-hidden="true" fill="none" focusable="false" height="1em" role="presentation" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="1em">
+                  <path d="M18 6L6 18M6 6l12 12"></path>
+                </svg>
+              </button>
+            )}
           </div>
+        )}
+
+        <div className={customClassNames.header ? customClassNames.header : 'pt-6 px-6 pb-4 flex-initial flex flex-col gap-2'}>
+          <h2 className='text-zinc-600 dark:text-white text-lg font-medium leading-4'>{title}</h2>
+          <p className='text-zinc-400 text-sm leading-3'>{subtitle}</p>
         </div>
-      )}
-    </>
+        <div className={`overflow-y-auto pt-4 pb-6 px-6 ${scrollBehavior === 'inside' ? 'max-h-full' : ''} ${customClassNames.body || ''}`}>
+          {children}
+        </div>
+      </div>
+    </div>,
+    portalContainer
   );
 };
 
