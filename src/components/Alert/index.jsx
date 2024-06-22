fb8f40d9160485d219a6b377bec94ceb41d6445a
Tamnora <tamnora@hotmail.com>
2024-06-22 20:55:24 -0300

    Ajustamos el estilo en AutoComplete y Alert

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { InfoIcon } from './InfoIcon';
 import { CloseIcon } from './CloseIcon';
 import { ErrorIcon } from './ErrorIcon';
 import { SuccessIcon } from './SuccessIcon';
 import { WarningIcon } from './WarningIcon';
 
-const Alert = (
-  {
-    message = '',
-    type = 'default',
-    radius = 'rounded-xl',
-    title = 'Titulo default',
-    icon = true,
-    onClose,
-  }) => {
-
+const Alert = ({
+  message = '',
+  type = 'default',
+  radius = 'rounded-xl',
+  title = 'Titulo default',
+  icon = true,
+  onClose,
+  timeOff,
+  position = 'top-right',
+}) => {
+  
   const alertType = {
     info: 'dark:text-blue-400 text-blue-800',
     danger: 'dark:text-red-400 text-red-800',
     success: 'dark:text-emerald-400 text-emerald-800',
     warning: 'dark:text-yellow-400 text-yellow-800',
-    default: 'dark:text-zinc-100 text-zinc-800'
+    default: 'dark:text-zinc-100 text-zinc-800',
   };
 
   const alertIcons = {
@@ -31,8 +32,25 @@ const Alert = (
     default: <InfoIcon />,
   };
 
+  const positionClasses = {
+    'top-left': 'top-3 left-3 tmn-fadeInDown',
+    'top-center': 'top-3 left-1/2 transform -translate-x-1/2 tmn-fadeInDown',
+    'top-right': 'top-3 right-3 tmn-fadeInDown',
+    'center': 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 tmn-fadeIn',
+    'bottom-left': 'bottom-3 left-3 tmn-fadeInUp',
+    'bottom-center': 'bottom-3 left-1/2 transform -translate-x-1/2 tmn-fadeInUp',
+    'bottom-right': 'bottom-3 right-3 tmn-fadeInUp',
+  };
+
+  useEffect(() => {
+    if (timeOff) {
+      const timer = setTimeout(onClose, timeOff);
+      return () => clearTimeout(timer);
+    }
+  }, [timeOff, onClose]);
+
   return (
-    <div className={`bg-white/60 dark:bg-black/60 backdrop-filter min-w-80 tmn-fadeInDown backdrop-blur-xl p-3 flex overflow-hidden ${message == '' ? 'items-center' : 'items-start'} justify-between fixed top-3 right-3 min-w-56 max-w-96 shadow-xl ${alertType[type]} ${radius}`} role="alert">
+    <div className={`bg-white/60 dark:bg-black/60 backdrop-filter min-w-96  backdrop-blur-xl p-3 flex overflow-hidden ${message === '' ? 'items-center' : 'items-start'} justify-between fixed ${positionClasses[position]} min-w-56 max-w-96 shadow-xl ${alertType[type]} ${radius}`} role="alert">
       <div className='flex flex-col'>
         <h3 className='flex items-center'>
           {icon && alertIcons[type]}
--- src/components/AutoComplete/index.jsx
+++ src/components/AutoComplete/index.jsx
@@ -2,90 +2,92 @@ import React, { useState } from 'react';
 import { Input } from '../Input';
 
 const AutoComplete = ({
-   data, 
-   columnSearch, 
-   sarta = false, 
-   keyReturn,
-   emptyData="No hay sugerencias",
-    ...props
-  }) => {
-  const [query, setQuery] = useState('');
-  const [filteredData, setFilteredData] = useState([]);
-  const [activeSuggestion, setActiveSuggestion] = useState(0);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
+  data, 
+  columnSearch, 
+  sarta = false, 
+  keyReturn,
+  emptyData = "No hay sugerencias",
+  enforceSelection = false,
+  startLetter = 0,
+   ...props
+ }) => {
+ const [query, setQuery] = useState('');
+ const [filteredData, setFilteredData] = useState([]);
+ const [activeSuggestion, setActiveSuggestion] = useState(0);
+ const [showSuggestions, setShowSuggestions] = useState(false);
+ const [selectedItem, setSelectedItem] = useState(null);
 
-  const handleChange = (e) => {
-    const userInput = e.target.value;
-    setQuery(userInput);
-    if (userInput.length > 0) {
-      const filtered = data.filter(item =>
-        sarta
-          ? item[columnSearch].toLowerCase().includes(userInput.toLowerCase())
-          : item[columnSearch].toLowerCase().startsWith(userInput.toLowerCase())
-      );
-      setFilteredData(filtered);
-      setShowSuggestions(true);
-    } else {
-      setShowSuggestions(false);
-    }
-  };
+ const handleChange = (e) => {
+   const userInput = e.target.value;
+   setQuery(userInput);
+   if (userInput.length > 0) {
+     const filtered = data.filter(item =>
+       sarta
+         ? item[columnSearch].toLowerCase().includes(userInput.toLowerCase())
+         : item[columnSearch].toLowerCase().startsWith(userInput.toLowerCase())
+     );
+     setFilteredData(filtered);
+     setShowSuggestions(true);
+   } else {
+     setShowSuggestions(false);
+   }
+ };
 
-  const handleClick = (item) => {
-    console.log(item)
-    setQuery(item[columnSearch]);
-    console.log('item', item[columnSearch])
-    setSelectedItem(item);
-    setShowSuggestions(false);
-    console.log(item[keyReturn]);
-  };
+ const handleClick = (item) => {
+   console.log(item)
+   setQuery(item[columnSearch]);
+   console.log('item', item[columnSearch])
+   setSelectedItem(item);
+   setShowSuggestions(false);
+   console.log(item[keyReturn]);
+ };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'ArrowDown') {
-      if (activeSuggestion < filteredData.length - 1) {
-        setActiveSuggestion(activeSuggestion + 1);
-      }
-    } else if (e.key === 'ArrowUp') {
-      if (activeSuggestion > 0) {
-        setActiveSuggestion(activeSuggestion - 1);
-      }
-    } else if (e.key === 'Enter') {
-      if (filteredData.length > 0) {
-        handleClick(filteredData[activeSuggestion]);
-      }
-    }
-  };
+ const handleKeyDown = (e) => {
+   if (e.key === 'ArrowDown') {
+     if (activeSuggestion < filteredData.length - 1) {
+       setActiveSuggestion(activeSuggestion + 1);
+     }
+   } else if (e.key === 'ArrowUp') {
+     if (activeSuggestion > 0) {
+       setActiveSuggestion(activeSuggestion - 1);
+     }
+   } else if (e.key === 'Enter') {
+     if (filteredData.length > 0) {
+       handleClick(filteredData[activeSuggestion]);
+     }
+   }
+ };
 
-  return (
-    <div className="relative w-full">
-      <Input
-        type="text"
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        value={query}
-        {...props}
-      />
-      {showSuggestions && query && (
-        <ul className="absolute z-20 mt-2 w-full bg-white dark:bg-zinc-900 shadow-lg rounded-xl p-2 text-sm text-zinc-700 dark:text-zinc-200">
-          {filteredData.length > 0 ? (
-            filteredData.map((item, index) => (
-              <li
-                key={item[keyReturn]}
-                className={`cursor-pointer px-2 py-1.5 hover:bg-zinc-200 rounded-lg dark:hover:bg-zinc-700 ${
-                  index === activeSuggestion ? 'bg-zinc-200 dark:bg-zinc-700' : ''
-                }`}
-                onClick={() => handleClick(item)}
-              >
-                {item[columnSearch]}
-              </li>
-            ))
-          ) : (
-            <li className="px-2 py-1.5 hover:bg-zinc-200 rounded-lg dark:hover:bg-zinc-700">{emptyData}</li>
-          )}
-        </ul>
-      )}
-    </div>
-  );
+ return (
+   <div className="relative w-full">
+     <Input
+       type="text"
+       onChange={handleChange}
+       onKeyDown={handleKeyDown}
+       value={query}
+       {...props}
+     />
+     {showSuggestions && query && (
+       <ul className="absolute z-20 mt-2 w-full bg-zinc-100 dark:bg-zinc-800 shadow-lg rounded-xl p-2 text-sm text-zinc-700 dark:text-zinc-300 border border-zinc-300 dark:border-zinc-600">
+         {filteredData.length > startLetter ? (
+           filteredData.map((item, index) => (
+             <li
+               key={item[keyReturn]}
+               className={`cursor-pointer px-2 py-1.5 hover:bg-zinc-200 rounded-lg  ${
+                 index === activeSuggestion ? 'bg-sky-100 dark:bg-sky-700 text-sky-700 dark:text-sky-200' : ''
+               }`}
+               onClick={() => handleClick(item)}
+             >
+               {item[columnSearch]}
+             </li>
+           ))
+         ) : (
+           <li className="px-2 py-1.5 bg-red-200 text-red-700 rounded-lg dark:bg-red-700 dark:text-red-200">{emptyData}</li>
+         )}
+       </ul>
+     )}
+   </div>
+ );
 };
 
 export {AutoComplete};
