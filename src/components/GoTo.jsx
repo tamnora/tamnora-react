f66dc0a9c783f471fb37a2277868eb32f3639a14
Tamnora <tamnora@hotmail.com>
2024-12-18 18:31:40 -0300

    Posibilidad de GoTo a pagina anterior

--- src/components/GoTo.jsx
+++ src/components/GoTo.jsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const GoTo = ({
   label = 'Volver',
   to = '/',
   icon,
   iconPosition = 'left',
-  className = 'flex items-center gap-1 text-sm tmn-fadeIn text-zinc-700 dark:text-zinc-400 hover:text-lime-600 dark:hover:text-lime-500',
+  className = 'flex items-center gap-1 text-sm tmn-fadeIn text-zinc-700 dark:text-zinc-400 hover:text-lime-600 dark:hover:text-lime-500 cursor-pointer',
 }) => {
+  const navigate = useNavigate();
+  
+   
   const iconElement = icon ? (
     <div className={`flex ${iconPosition === 'top' || iconPosition === 'bottom' ? 'flex-col items-center justify-between' : 'flex-row justify-center items-center'}`}>
       {iconPosition === 'top' && icon}
@@ -19,11 +22,26 @@ export const GoTo = ({
     <span className='font-semibold'>{label}</span>
   );
 
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <Link to={to} >
+    <>
+    { to != '..' && 
+      <Link to={to} >
       <div className={className}>
         {iconElement}
       </div>
     </Link>
+    }
+    { to == '..' &&
+      <div className={className} onClick={handleGoBack} >
+        {iconElement}
+      </div>
+    }
+    
+    
+    </>
   )
 }
